2d5d7f31eb12c9297719ed37c2feba079dbee892
Warith Lawal <79901863+Warithlawal@users.noreply.github.com>
2025-06-21 18:02:32 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -38,3 +38,15 @@ faqLinks.forEach(link => {
     e.currentTarget.classList.add('active');
   });
 });
+
+
+
+const toggle = document.getElementById("menu-toggle");
+  const menu = document.getElementById("nav-menu");
+  const icon = toggle.querySelector("i");
+
+  toggle.addEventListener("click", () => {
+    menu.classList.toggle("show");
+    icon.classList.toggle("fa-bars");
+    icon.classList.toggle("fa-xmark");
+  });
