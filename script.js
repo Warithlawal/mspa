1556818615db47b5a52be8678c6706ef976ffb4e
Warith Lawal <79901863+Warithlawal@users.noreply.github.com>
2025-06-24 15:27:59 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -50,3 +50,40 @@ const toggle = document.getElementById("menu-toggle");
     icon.classList.toggle("fa-bars");
     icon.classList.toggle("fa-xmark");
   });
+
+
+
+
+  const contactForm = document.getElementById('applicationForm'),
+      contactMessage = document.getElementById('contact-message'),
+      application = document.getElementById('form_container');
+
+const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_6spnte3', 'template_fuq62i5', contactForm, 'uSqokHFUsvySxcuIr')
+    .then(() => {
+        application.style.display = 'none';  // Hide the form
+        contactMessage.style.display = 'block'; // Show the message
+
+        setTimeout(() => {
+            contactMessage.style.display = 'none';
+            contactMessage.textContent = ''; // Clear the message text
+        }, 5000);
+
+        contactForm.reset();
+    })
+    .catch((error) => {
+        contactMessage.style.display = 'block'; // Show the message
+        contactMessage.innerHTML = '<img src="images/error.png" alt="Error"><span>Failed to send application. Please try again later.</span>';
+
+        setTimeout(() => {
+            contactMessage.style.display = 'none';
+            contactMessage.innerHTML = ''; // Clear the message text
+        }, 5000);
+    });
+};
+
+contactForm.addEventListener('submit', sendEmail);
+
+
